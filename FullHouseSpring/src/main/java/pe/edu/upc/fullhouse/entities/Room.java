c56b26f1909e2d1976cee4051ed1b4676453e51f
CZLLBastian <86624656+CZLLBastian@users.noreply.github.com>
2022-06-16 09:38:40 -0500

    Update Room.java

--- FullHouseSpring/src/main/java/pe/edu/upc/fullhouse/entities/Room.java
+++ FullHouseSpring/src/main/java/pe/edu/upc/fullhouse/entities/Room.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -14,21 +16,33 @@ public class Room {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idRoom;
+	
 	@Column(name = "descpRoom", nullable = false, length = 100)
 	private String descpRoom;
+	
 	@Column(name = "adress", nullable =false , length = 40)
 	private String adress;
+	
+	@ManyToOne
+	@JoinColumn(name = "idArrendador")
+	private Arrendador arrendador;
+	
+	@ManyToOne
+	@JoinColumn(name = "idDistrict")
+	private District distrito;
 	
 	public Room() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Room(int idRoom, String descpRoom, String adress) {
+	public Room(int idRoom, String descpRoom, String adress, Arrendador arrendador, District distrito) {
 		super();
 		this.idRoom = idRoom;
 		this.descpRoom = descpRoom;
 		this.adress = adress;
+		this.arrendador = arrendador;
+		this.distrito = distrito;
 	}
 
 	public int getIdRoom() {
@@ -53,9 +67,22 @@ public class Room {
 
 	public void setAdress(String adress) {
 		this.adress = adress;
-	} 
-	
-		
-	
+	}
+
+	public Arrendador getArrendador() {
+		return arrendador;
+	}
+
+	public void setArrendador(Arrendador arrendador) {
+		this.arrendador = arrendador;
+	}
+
+	public District getDistrito() {
+		return distrito;
+	}
+
+	public void setDistrito(District distrito) {
+		this.distrito = distrito;
+	}
 	
 }
