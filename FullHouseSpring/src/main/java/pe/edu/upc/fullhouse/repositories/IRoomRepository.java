18717018ef96c3ec06a4d4922b22af7a489c30ae
MiriamMamaniChullo <103612596+MiriamMamaniChullo@users.noreply.github.com>
2022-06-28 10:03:52 -0500

    Update IRoomRepository.java

--- FullHouseSpring/src/main/java/pe/edu/upc/fullhouse/repositories/IRoomRepository.java
+++ FullHouseSpring/src/main/java/pe/edu/upc/fullhouse/repositories/IRoomRepository.java
@@ -1,11 +1,17 @@
 package pe.edu.upc.fullhouse.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.fullhouse.entities.Room;
 
 @Repository
 public interface IRoomRepository extends JpaRepository<Room, Integer> {
-
+	
+	//reporte
+		@Query(value="Select r.descp_room, d.nom_district, r.adress,v.pre_habitacion from room r join district d on d.id_district=r.id_district inner join aviso v on v.id_habitacion=r.id_room group by r.descp_room, d.nom_district,r.adress,v.pre_habitacion order by 4 asc", nativeQuery=true)
+		public List<String[]>roomPrice();
 }
